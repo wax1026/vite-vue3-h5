b8097106a646074ec57fdde753770aa42da6f8df
wax <402699867@qq.com>
2022-01-17 16:06:52 +0800

    #0117优化config设置，开启ip

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,39 @@
-import { defineConfig, loadEnv } from 'vite'
-import path from 'path'
-import createVitePlugins from './vite/plugins'
+import { defineConfig, loadEnv } from "vite";
+import path from "path";
+import createVitePlugins from "./vite/plugins";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
-  const env = loadEnv(mode, process.cwd())
+  const env = loadEnv(mode, process.cwd());
   return {
-    base: './', // 开发或生产环境服务的公共基础路径
-    plugins: createVitePlugins(env, command === 'build'),
+    base: "./", // 开发或生产环境服务的公共基础路径
+    plugins: createVitePlugins(env, command === "build"),
     resolve: {
       // https://cn.vitejs.dev/config/#resolve-alias
       alias: {
         // 设置路径
-        '~': path.resolve(__dirname, './'),
+        "~": path.resolve(__dirname, "./"),
         // 设置别名
-        '@': path.resolve(__dirname, './src')
+        "@": path.resolve(__dirname, "./src")
       },
       // https://cn.vitejs.dev/config/#resolve-extensions
-      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+      extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"]
     },
     // vite 相关配置
     server: {
+      host: "0.0.0.0",
       port: 8000,
+      // 是否开启 https
+      https: false,
       open: false,
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
-        '/dev-api': {
-          target: 'http://localhost:8080',
+        "/dev-api": {
+          target: "http://localhost:8080",
           changeOrigin: true,
-          rewrite: (p) => p.replace(/^\/dev-api/, '')
+          rewrite: (p) => p.replace(/^\/dev-api/, "")
         }
-      },
-    },
-  }
-})
+      }
+    }
+  };
+});
