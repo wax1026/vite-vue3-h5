4e6fe36f8299ba638a189dd0c2006454a816a3ea
wax <402699867@qq.com>
2022-01-17 16:07:32 +0800

    #0117优化vant的组件相关逻辑

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,11 +1,70 @@
 // vant 按需引入
-import { Button, Form, Field, CellGroup, Dialog, Notify } from 'vant';
+// import 'vant/lib/index.css';
+import "@vant/touch-emulator"; // 桌面端touch适配
+import {
+  Button,
+  List,
+  Cell,
+  Tabbar,
+  TabbarItem,
+  Swipe,
+  SwipeCell,
+  SwipeItem,
+  Grid,
+  GridItem,
+  NavBar,
+  Icon,
+  Sticky,
+  Card,
+  Sidebar,
+  SidebarItem,
+  Form,
+  Field,
+  CellGroup,
+  Checkbox,
+  CheckboxGroup,
+  Tag,
+  Image,
+  Empty,
+  Search,
+  Popup,
+  Loading,
+  Divider
+} from "vant";
+
+const plugins = [
+  Button,
+  List,
+  Cell,
+  Tabbar,
+  TabbarItem,
+  Swipe,
+  SwipeCell,
+  SwipeItem,
+  Grid,
+  GridItem,
+  NavBar,
+  Icon,
+  Sticky,
+  Card,
+  Sidebar,
+  SidebarItem,
+  Form,
+  Field,
+  CellGroup,
+  Checkbox,
+  CheckboxGroup,
+  Tag,
+  Image,
+  Empty,
+  Search,
+  Popup,
+  Loading,
+  Divider
+];
 
 export default (app) => {
-  app.use(Button);
-  app.use(Form);
-  app.use(Field);
-  app.use(CellGroup);
-  app.use(Dialog);
-  app.use(Notify);
+  plugins.forEach((item) => {
+    app.use(item);
+  });
 };
